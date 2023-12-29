df258ce6106842add5972bc3b29a4a2c0864be29
Restyled.io <commits@restyled.io>
2023-12-29 18:44:15 +0000

    Restyled by shfmt

--- hack/verify-license.sh
+++ hack/verify-license.sh
@@ -4,15 +4,15 @@
 set -e
 
 array_contains() {
-    local search="$1"
-    local element
-    shift
-    for element; do
-        if [[ "${element}" == "${search}" ]]; then
-            return 0
-        fi
-    done
-    return 1
+  local search="$1"
+  local element
+  shift
+  for element; do
+    if [[ "${element}" == "${search}" ]]; then
+      return 0
+    fi
+  done
+  return 1
 }
 
 HEADER="SPDX-License-Identifier: MIT"
@@ -24,25 +24,25 @@ unset IFS
 
 errors=()
 for file in "${all_error_files[@]}"; do
-    array_contains "$file" "${HEADER_WHITELIST[@]}" && in_whitelist=$? || in_whitelist=$?
-    if [[ "${in_whitelist}" -eq "0" ]]; then
-        continue
-    fi
-    set +e
-    matches=$(head -n 2 "$file" | grep "$HEADER" | wc -l)
-    set -e
-    if [[ "$matches" -ne "1" ]]; then
-        errors+=("$file")
-        echo "error checking $file for the SPDX header"
-    fi
+  array_contains "$file" "${HEADER_WHITELIST[@]}" && in_whitelist=$? || in_whitelist=$?
+  if [[ "${in_whitelist}" -eq "0" ]]; then
+    continue
+  fi
+  set +e
+  matches=$(head -n 2 "$file" | grep "$HEADER" | wc -l)
+  set -e
+  if [[ "$matches" -ne "1" ]]; then
+    errors+=("$file")
+    echo "error checking $file for the SPDX header"
+  fi
 done
 
 if [ ${#errors[@]} -eq 0 ]; then
-    echo 'Congratulations! All source files have been checked for SPDX header.'
+  echo 'Congratulations! All source files have been checked for SPDX header.'
 else
-    echo
-    echo 'Please review the above files. They seem to miss the following header as comment:'
-    echo "$HEADER"
-    echo
-    exit 1
+  echo
+  echo 'Please review the above files. They seem to miss the following header as comment:'
+  echo "$HEADER"
+  echo
+  exit 1
 fi
